ae22ce04a1f86f6e4697f8e8fdc624df8181f91b
rajeshranjan28 <rajeshranjan2803@gmail.com>
2025-07-22 23:39:15 +0530

    Add all project files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import './index.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1 className='text-red-700 bg-blue-500'>hello world </h1>
+    </>
+  )
+}
+
+export default App
